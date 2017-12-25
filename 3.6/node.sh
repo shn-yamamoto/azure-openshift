a517f6e4ced4951d654ec21e512229b54e6e6c1a
Shingo Yamamoto <shingo.yamamoto.zr@hitachi-solutions.com>
2017-12-25 00:39:43 +0000

    change repo rhel-7-server-ose-3.5-rpms to ver.3.6

--- 3.6/node.sh
+++ 3.6/node.sh
@@ -18,7 +18,7 @@ subscription-manager repos --disable="*"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.5-rpms" \
+    --enable="rhel-7-server-ose-3.6-rpms" \
     --enable="rhel-7-fast-datapath-rpms"
 
 #yum -y update
